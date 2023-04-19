a2e6a2a8c4084876684c366098a4d5551af48992
Abhishek venkata <abhishekvenkata.199715@gmail.com>
2023-04-18 21:26:50 -0400

    changing km to mts in output

--- src/main/java/com/tsp/TSP_Spring/TSP.java
+++ src/main/java/com/tsp/TSP_Spring/TSP.java
@@ -24,8 +24,6 @@ public class TSP {
         mst.primMST(graph);
         System.out.println("MST Tour Ended..");
 
-
-
         System.out.println("Christofides Tour Start");
         int[] christofidesSolution = Christofides.applyChristofidesAlgorithm(graph);
         List<Integer> christofidesList = new ArrayList<>();
@@ -51,7 +49,7 @@ public class TSP {
             System.out.print(", Longitude: " + coOrdinates.get(i)[0]);
             System.out.println();
         }
-        System.out.println("2-opt distance=" + TourDistance.tourDistance(twoOptNewList, graph));
+        System.out.println("2-opt distance=" + TourDistance.tourDistance(twoOptNewList, graph) * 1000);
         System.out.println("Two-opt Tour Ended..");
 
         System.out.println("Three-opt Tour Start..");
@@ -141,7 +139,7 @@ public class TSP {
         if (a == 2) {
             List<Integer> threeOptNewList = new ArrayList<>();
             threeOptNewList = ThreeOpt.threeOptAlgorithm(christofidesList, graph);
-            System.out.print("Three opt tour distance: " + TourDistance.tourDistance(threeOptNewList, graph));
+            System.out.print("Three opt tour distance: " + TourDistance.tourDistance(threeOptNewList, graph) * 1000);
             return threeOptNewList;
         }
         // Ant Colony Optimization
@@ -160,7 +158,7 @@ public class TSP {
             for (int i = 0; i < acoTour.length; i++) {
                 ACONewList.add(acoTour[i]);
             }
-            System.out.println("Length of best tour ACO: " + TourDistance.tourDistance(ACONewList, graph));
+            System.out.println("Length of best tour ACO: " + TourDistance.tourDistance(ACONewList, graph) * 1000);
             return ACONewList;
 
         }
@@ -169,8 +167,8 @@ public class TSP {
             Collections.shuffle(christofidesList);
             SimulatedAnnealing sa = new SimulatedAnnealing(graph, christofidesList);
             List<Integer> bestTour = sa.solve(10000000, 100000, 0.995);
-            System.out.println("Initial distance: " + sa.calculateTourDistance(christofidesList));
-            System.out.println("Tour distance: " + sa.calculateTourDistance(bestTour));
+            System.out.println("Initial distance: " + sa.calculateTourDistance(christofidesList) * 1000);
+            System.out.println("Tour distance: " + sa.calculateTourDistance(bestTour) * 1000);
             return bestTour;
         }
         return christofidesList;
